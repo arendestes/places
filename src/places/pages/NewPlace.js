d195fdc0c8d7d38c74264540e1d892c4c6df9a11
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-07-01 08:43:05 -0500

    created state and functionality for input component with useReducer

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,8 +5,9 @@ import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
 
 const NewPlace = () => {
+
     return <form className='place-form'>
-        <Input type='text' label='Title' element='input' />
+        <Input type='text' label='Title' element='input' validators={[]} errorText='Please enter a valid title.' />
     </form>
 }
 
