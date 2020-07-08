7ef230835ce3cc1c47ad664c93761300a94c6c6b
Arend Estes <42525228+arendestes@users.noreply.github.com>
2020-07-08 14:56:40 -0500

    used creatContext to make an AuthContext to have an app wide state to manage to check if a user is logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
@@ -6,33 +6,51 @@ import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces'
 import UpdatePlace from './places/pages/UpdatePlace';
-import Auth from './user/pages/Auth'
+import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 
 function App() {
-  return <Router>
-    <MainNavigation />
-    <main>
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path='/:userID/places' exact>
-          <UserPlaces />
-        </Route>
-        <Route path='/places/new' exact>
-          <NewPlace />
-        </Route>
-        <Route path='/place/:placeID'>
-          <UpdatePlace />
-        </Route>
-        <Route path='/auth'>
-          <Auth />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </main>
-  </Router>
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const login = useCallback(
+    () => {
+      setLoggedIn(true);
+    }, [],
+  )
+
+  const logout = useCallback(
+    () => {
+      setLoggedIn(false);
+    }, [],
+  )
+
+  return <AuthContext.Provider>
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Route path='/:userID/places' exact>
+            <UserPlaces />
+          </Route>
+          <Route path='/places/new' exact>
+            <NewPlace />
+          </Route>
+          <Route path='/place/:placeID'>
+            <UpdatePlace />
+          </Route>
+          <Route path='/auth'>
+            <Auth />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  </AuthContext.Provider>
 }
 
 export default App;
